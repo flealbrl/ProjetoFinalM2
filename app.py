316e6bcf7f2d960f4069ca5ab7408c762b49823c
antonio00 <marcos_leite00@hotmail.com>
2021-08-09 19:02:33 -0300

    A

--- app.py
+++ app.py
@@ -16,28 +16,22 @@ class Portifolio(db.Model):
     descricao = db.Column(db.String(500), nullable=False)
     video = db.Column(db.String(500), nullable=False)
     autor = db.Column(db.String(150), nullable=False)
-
     def __init__(self, nome, imagem, descricao, video, autor):
         self.nome = nome
         self.imagem = imagem
         self.descricao = descricao
         self.video = video
         self.autor = autor
-
-
-
 ### Criando rotas abaixo###
 @app.route('/') # renderiza a pagina principal(rota)
 def index():
     session['usuario_logado'] = None # sempre que for para rota principal, 'desloga' o usuario
     return render_template('index.html')
-
 #######
 @app.route('/login')
 def login():
     session['usuario_logado'] = None
     return render_template('login.html')
-
 @app.route('/auth', methods=['GET', 'POST']) # Rota de autenticação
 def auth():
     if request.form['senha'] == 'admin': # Se a senha for 'admin' faça:
@@ -46,14 +40,11 @@ def auth():
         return redirect('/adm') # Redireciona para a rota adm
     else: # Se a senha estiver errada, faça:
         flash('Erro no login, tente novamente!')  # Envia mensagem de erro
-        return redirect('/login') # Redireciona para a rota login
-    
-
+        return redirect('/login') # Redireciona para a rota login   
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     return redirect('/login')
-
 #PAINEL ADMINISTRATIVO#
 @app.route('/adm')
 def adm():
@@ -67,7 +58,6 @@ def adm():
     #    return redirect('/login')
     # portifolio = Portifolio.query.all() #vai ler tudo, select * from projetos
     # return render_template('adm.html', portifolio= portifolio, portifolio='')
-
 ### create##### CRIAR/ADD ITEM
 @app.route('/new', methods=['GET', 'POST'])
 def new(): # new definido no formulario de inclusao na pagina adm
@@ -84,7 +74,6 @@ def new(): # new definido no formulario de inclusao na pagina adm
         db.session.commit() # Confirma a operação
         flash('Item Adicionado com suceso!')
         return redirect('/adm') 
-
 #### EDITAR ####
 @app.route('/edit/<id>', methods=['GET', 'POST'])
 def edit(id):
@@ -101,8 +90,6 @@ def edit(id):
         return redirect('/adm') #Redireciona para a rota adm
         # Renderiza a página adm.html passando o projetoEdit (projeto a ser editado)
     return render_template('edit.html', portifolio=portifolio) 
-
-
 @app.route("/<id>")
 def get_by_id(id):
     portifolio = Portifolio.query.get(id)
@@ -116,42 +103,32 @@ def delete(id):
     db.session.commit()
     flash('Item Deletado com sucesso!')
     return redirect('/adm')
-
-
 #######
 @app.route('/antonio') # renderiza a pagina (rota)
 def antonio():
     # portifolio = Portifolio.query.all()
     portifolio = Portifolio.query.filter(Portifolio.autor.ilike('%antonio%')) #faz o filtro 
     return render_template('antonio.html', portifolio=portifolio )
-
 @app.route('/elisama') # renderiza a pagina (rota)
 def elisama():
     # projetos = Projeto.query.all()
     portifolio = Portifolio.query.filter(Portifolio.autor.ilike('%Elisama%')) #faz o filtro
     return render_template('elisama.html', portifolio=portifolio)
-
 @app.route('/fellipe') # renderiza a pagina(rota)
 def fellipe():
     # projetos = Projeto.query.all()
     portifolio = Portifolio.query.filter(Portifolio.autor.ilike('%fellipe%')) #faz o filtro
     return render_template('fellipe.html', portifolio=portifolio)
-
-
 @app.route('/rudhy') # renderiza a pagina principal(rota)
 def rudhy():
     # projetos = Projeto.query.all()
     portifolio = Portifolio.query.filter(Portifolio.autor.ilike('%rudhy%')) #faz o filtro
     return render_template('rudhy.html', portifolio=portifolio)
-
-
 @app.route('/sobre') # renderiza a pagina (rota)
 def sobre():
     # portifolio = Portifolio.query.all()
     portifolio = Portifolio.query.filter(Portifolio.autor.ilike('%sobre%')) #faz o filtro 
     return render_template('sobre.html', portifolio=portifolio )
-
-
 
 
 if __name__ == '__main__':
